690a1a386b05db077588f1126a75779e9d382094
Unknown <alsomsau@gmail.com>
2018-03-22 11:39:03 +0700

    create view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    <h3>Analysis Stck</h3>
-                   <ul>
-                       <li><a href="{{route('peg')}}">PEG</a></li>
-                       <li><a href="{{route('ddm')}}">DDM</a></li>
-                       <li><a href="{{route('dcf')}}">DCF</a></li>
-                       <li><a href="{{route('ps')}}">PS</a></li>
-                       <li><a href="{{route('ev')}}">EV/Ebitda</a></li>
-                   </ul>
+                        <h3>Analysis Stck</h3>
+                        <ul>
+                            <li><a href="{{route('peg')}}">PEG</a></li>
+                            <li><a href="{{route('ddm')}}">DDM</a></li>
+                            <li><a href="{{route('dcf')}}">DCF</a></li>
+                            <li><a href="{{route('ps')}}">PS</a></li>
+                            <li><a href="{{route('ev')}}">EV/Ebitda</a></li>
+                        </ul>
 
                         <h3>Find value</h3>
                         <ul>
@@ -34,9 +34,9 @@
                             <li><a href="{{route('peg')}}">????????????</a></li>
 
                         </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
